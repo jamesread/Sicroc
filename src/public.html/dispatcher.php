1e5274afe8bacf33adb93e37c3abe3eb1f7f4562
jamesread <contact@jwread.com>
2015-05-23 10:30:31 +0100

    Fixed the include path so it can be included from various places

--- src/public.html/dispatcher.php
+++ src/public.html/dispatcher.php
@@ -1,6 +1,6 @@
 <?php
 
-require_once '../private/common.php';
+require_once __DIR__ . '/../private/common.php';
 
 $principle = san()->filterString('controller');
 $method = san()->filterString('method');
