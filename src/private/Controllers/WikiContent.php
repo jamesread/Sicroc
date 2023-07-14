0676492b4d429a7c690b09f1005518e815989369
jamesread <contact@jread.com>
2023-07-14 11:44:29 +0100

    Fixed issues with wiki

--- src/private/Controllers/WikiContent.php
+++ src/private/Controllers/WikiContent.php
@@ -4,24 +4,52 @@ namespace Sicroc\Controllers;
 
 use \libAllure\DatabaseFactory;
 
+use function libAllure\util\db;
+
 class WikiContent extends Widget
 {
+
+    public function getPage($pageTitle)
+    {
+        $sql = 'SELECT w.principle, w.content FROM wiki_content w WHERE w.principle = :principle';
+        $stmt = db()->prepare($sql);
+        $stmt->bindValue(':principle', $pageTitle);
+        $stmt->execute();
+
+        return $stmt->fetchRow();
+    }
+
+    public function createWikiPage($pageTitle)
+    {
+        $sql = 'INSERT INTO wiki_content (principle) VALUES (:principle) ';
+        $stmt = db()->prepare($sql);
+        $stmt->bindValue(':principle', $pageTitle);
+        $stmt->execute();
+    }
+
     public function view()
     {
         global $tpl;
 
-        $sql = 'SELECT w.principle, w.content FROM wiki_content w WHERE w.principle = :principle';
-        $stmt = DatabaseFactory::getInstance()->prepare($sql);
-        $stmt->bindValue(':principle', $this->principle);
-        $stmt->execute();
+        $pageTitle = $this->getArgumentValue('pageTitle');
 
-        $wiki = $stmt->fetchRow();
+        if ($pageTitle == null) {
+            $wiki = [];
+        } else {
+            $wiki = $this->getPage($pageTitle);
 
-        if ($wiki != false) {
-            $wiki['content'] = str_replace("\n\n", '<br />', $wiki['content']);
+            if ($wiki === false) {
+                $this->createWikiPage($pageTitle);
+                $wiki = $this->getPage($pageTitle);
+            }
+
+            \libAllure\util\vde($wiki);
+            if ($wiki != false) {
+                $wiki['content'] = str_replace("\n\n", '<br />', $wiki['content']);
+            }
         }
 
-        $this->navigation->add('dispatcher.php?controller=WikiContent&amp;pageIdent=WIKI_EDIT&amp;pageTitle=' . $this->principle, 'Edit');
+        $this->navigation->add('dispatcher.php?controller=WikiContent&amp;pageIdent=WIKI_EDIT&amp;pageTitle=' . $pageTitle, 'Edit');
         $tpl->assign('wikiPage', $wiki);
     }
 
@@ -34,6 +62,19 @@ class WikiContent extends Widget
     function display()
     {
         return $this->view();
+    }
+
+    function getTitle()
+    {
+        return 'Wiki content';
+    }
+
+    function getArguments()
+    {
+        $args = array();
+        $args[] = array('type' => 'varchar', 'name' => 'pageTitle', 'default' => '', 'description' => 'The page name');
+
+        return $args;
     }
 
 }
