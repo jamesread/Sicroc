1c8054e389665d517234cdff312941c88c403024
jamesread <contact@jread.com>
2023-09-03 21:24:11 +0100

    Script for table sorting

---
+++ src/public.html/resources/js/main.js
@@ -0,0 +1,35 @@
+function sortColumn(tbl, th, idx) {
+  const tbody = tbl.querySelector('tbody');
+  let rows = [];
+
+  const asc = th.classList.contains('sortDesc');
+  
+  for (const tr of tbody.querySelectorAll('tr')) {
+    rows.push(tr);
+    tr.remove();
+  }
+
+  rows.sort((a, b) => {
+    const av = a.querySelectorAll('td')[idx].innerText;
+    const bv = b.querySelectorAll('td')[idx].innerText;
+
+    if (asc) {
+      return av.localeCompare(bv, 'en', {numeric: true});
+    } else {
+      return bv.localeCompare(av, 'en', {numeric: true});
+    }
+  });
+
+  for (row of rows) {
+    tbody.appendChild(row);
+  }
+
+  th.classList.remove('sortAsc');
+  th.classList.remove('sortDesc');
+
+  if (asc) {
+    th.classList.add('sortAsc');
+  } else {
+    th.classList.add('sortDesc');
+  }
+}
